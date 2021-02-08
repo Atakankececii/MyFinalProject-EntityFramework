7aa7fc558332f89551cacc2853c5b272dbd50cac
Süleyman Atakan Keçeci <atakan_kececi48@hotmail.com>
2021-02-08 18:37:57 +0300

    6 Şubat Yazılım Geliştiric Kampı ders sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,25 +9,29 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            ProductTest();
+            //CategoryTest();
+
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            //foreach (var product in productManager.GetAll())
-            //{
-            //    Console.WriteLine(product.ProductName);
-            //}
 
-            //foreach (var product in productManager.GetAllByCategoryId(2))
-            //{
-            //    Console.WriteLine(product.ProductName);
-            //}
-
-            foreach (var product in productManager.GetByUnitPrice(10,20))
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName+"/"+product.CategoryName);
             }
-
-
-
         }
     }
 }
--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -1,4 +1,6 @@
-﻿using Entities.Concrete;
+﻿using Core.DataAccess;
+using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,6 +10,6 @@ namespace DataAccess.Abstract
     //Interface'in kendisi public değildir ama içindeki fonksiyonlar publictir.Bu yüzden fonksiyonların başına ğublic yazılmaz ancak interface'in başına yazılır.
     public interface IProductDal:IEntityRepository<Product>
     {
-      
+        List<ProductDetailDto> GetProductDetails(); 
     }
 }
